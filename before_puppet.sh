c1a74abd9a8880cb0adfeaa75117c99c36da70ff
Nic <bazy84@gmail.com>
2014-02-01 08:18:05 +0000

    added yum install git and puppet FTW :)

--- before_puppet.sh
+++ before_puppet.sh
@@ -6,6 +6,7 @@
 yum -y install http://ftp.astral.ro/mirrors/fedora/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum -y install http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 #
+yum -y install puppet git
 git clone https://github.com/bazy84/first-puppet.git /var/lib/puppet/code
 # first puppet run
 puppet apply --verbose /var/lib/puppet/code/manifests/site.pp --modulepath=/var/lib/puppet/code/modules/
