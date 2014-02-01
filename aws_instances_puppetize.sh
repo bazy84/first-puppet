9c2c4928be56c88e9c17c8efab97d2333ab2ea7b
Nicolae Critu <nicolae.critu@unifiedpost.com>
2014-02-01 22:17:37 +0200

    I had too much Monster energy! and wrote  instead on

--- aws_instances_puppetize.sh
+++ aws_instances_puppetize.sh
@@ -7,6 +7,6 @@ do
   ssh -o StrictHostKeyChecking=no root@${node} "stat /var/lib/puppet/code" &>/dev/null
     if [ $? -ne 0 ]
     then
-      scp -o StrictHostKeyChecking=no /home/upncritu/mine/UP/git/first-puppet/before_puppet.sh root@${host}:/root && ssh -o StrictHostKeyChecking=no -t root@${node} "/root/before_puppet.sh"
+      scp -o StrictHostKeyChecking=no /home/upncritu/mine/UP/git/first-puppet/before_puppet.sh root@${node}:/root && ssh -o StrictHostKeyChecking=no -t root@${node} "/root/before_puppet.sh"
     fi
 done
